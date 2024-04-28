84735d0d48dca7fcec43f75be0a0d8e76242e2dd
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-04-29 00:43:40 +0900

    バーナーの作成

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,19 @@
 import { requests } from "./request"
 import { Row } from "./components/Row"
+import { Banner } from "./components/Banner"
 
 function App() {
 
   return (
     <div className="App bg-slate-950">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-      <Row title="Trend Movies" fetchUrl={requests.fetchTrending} isLargeRow />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow/>
-      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} isLargeRow/>
-      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} isLargeRow/>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow/>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} isLargeRow/>
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+      <Row title="Trend Movies" fetchUrl={requests.fetchTrending} isLargeRow={false} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={false}/>
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} isLargeRow={false}/>
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} isLargeRow={false}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={false}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} isLargeRow={false}/>
     </div>
   )
 }
---
+++ vite-project/src/type.ts
@@ -0,0 +1,8 @@
+export type Movie = {
+  id: string;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+  overview: string;
+};
+

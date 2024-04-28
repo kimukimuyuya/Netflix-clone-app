077ca933b9499e4509f1223e41951fead8f20bff
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-04-29 00:03:40 +0900

    一覧画面作成

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,8 +4,14 @@ import { Row } from "./components/Row"
 function App() {
 
   return (
-    <div className="App">
-      <Row fetchUrl={requests.fetchNetflixOriginals} />
+    <div className="App bg-slate-950">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title="Trend Movies" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow/>
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} isLargeRow/>
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} isLargeRow/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} isLargeRow/>
     </div>
   )
 }

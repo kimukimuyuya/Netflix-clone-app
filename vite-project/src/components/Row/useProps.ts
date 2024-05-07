ba85903ab9f4443ea0603f0521de544b2c054f6b
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-05-07 11:59:46 +0900

    Youtubeの予告映像の表示

--- vite-project/src/components/Row/useProps.ts
+++ vite-project/src/components/Row/useProps.ts
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "../../axios";
 import  { Movie } from "../../type";
+import { requests } from "../../request";
 
 export const useProps = (fetchUrl: string) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request.data.results)
       const movies = request.data.results.map((movie: Movie) => ({
         id: movie.id,
         name: movie.name,
@@ -21,5 +22,18 @@ export const useProps = (fetchUrl: string) => {
     fetchData();
   }, [fetchUrl])
 
-  return movies;
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      const moviePlayUrl = await axios.get(requests.fetchMovieVideos(movie.id));
+      setTrailerUrl(moviePlayUrl.data.results[0]?.key);
+    }
+  }
+
+  return {
+    movies,
+    handleClick,
+    trailerUrl,
+  };
 };
